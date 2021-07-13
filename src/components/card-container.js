622828d924b2ead7e56c50308cc76b5f8442fb1f
joe <joe.maliksi@gmail.com>
2021-07-13 01:13:16 -0400

    use json-url again

--- src/components/card-container.js
+++ src/components/card-container.js
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from "uuid";
 import { useEffect } from "react";
 import { JSONCrush, JSONUncrush } from "jsoncrush";
 import { useHistory } from "react-router-dom";
+import codec from "json-url";
 
 const toTinyColumn = (column) => {
   let tiny = {};
@@ -47,16 +48,22 @@ const CardContainer = (props) => {
 
   useEffect(() => {
     if (props.match?.params.hash) {
+      /*
       const c = decompress(props.match.params.hash);
       c.map((col) => {
         addColumn(col);
       });
+      */
+      codec("lzma").decompress(props.match.params.hash).then((json) => json.map((j) => addColumn(fromTinyColumn(j))));
     }
   }, []);
 
   useEffect(() => {
+    /*
     let crushed = compress(columns);
     history.push(`/${crushed}`);
+    */
+    codec("lzma").compress(columns.map(toTinyColumn)).then((s) => history.push(`/${s}`));
   }, [columns]);
 
   return (
